e996d4c9e520a741533c01d5db66b1e2b529a60b
Vadim Kosarev <vkosarev@opendesign.com>
2022-02-06 21:26:07 +0300

    00.env

---
+++ download-premier-one/00.env.cmd
@@ -0,0 +1,3 @@
+set urlBase=https://video-1-201.uma.media/hls-vod/p1Hntzr4nN3AJK2aGze3_w/1644184453/67/0x5000c500b35da91f/9f321ab742784ab08907ff2da5332240.mp4
+set maxSegment=250
+set videoName=Вписка у Маргулиса - MC Сенечка
