8f9ed698a67330ee191bd18710e14936b82a171f
Vadim Kosarev <vkosarev@opendesign.com>
2023-11-14 19:23:04 +0300

    downloaded pp-67

--- download-vk-video/t.cmd
+++ download-vk-video/t.cmd
@@ -1,5 +1,6 @@
 set suffix=plohie-pesni-67
 
-curl -H @headers.txt https://vkvd214.mycdn.me/?expires=1700433809434^&srcIp=31.173.86.154^&pr=40^&srcAg=CHROME^&ms=45.136.22.203^&type=1^&sig=Ga2vUO7XFLQ^&ct=22^&urls=45.136.21.150^&clientType=13^&appId=512000384397^&id=4289981909641^&bytes=0-527738962 --output a-video-%suffix%.webm
+curl -H @headers.txt https://vkvd214.mycdn.me/?expires=1700433809434^&srcIp=31.173.86.154^&pr=40^&srcAg=CHROME^&ms=45.136.22.203^&type=6^&sig=SxwmA8rwjz4^&ct=21^&urls=45.136.21.150^&clientType=13^&appId=512000384397^&id=4289981909641^&bytes=0-52773896200 --output a-video-%suffix%.webm
 
-curl -H @headers.txt https://vkvd214.mycdn.me/?expires=1700433809434^&srcIp=31.173.86.154^&pr=40^&srcAg=CHROME^&ms=45.136.22.203^&type=6^&sig=SxwmA8rwjz4^&ct=21^&urls=45.136.21.150^&clientType=13^&appId=512000384397^&id=4289981909641^&bytes=0-29594234 --output b-audio-%suffix%.webm
+curl -H @headers.txt https://vkvd214.mycdn.me/?expires=1700433809434^&srcIp=31.173.86.154^&pr=40^&srcAg=CHROME^&ms=45.136.22.203^&type=1^&sig=Ga2vUO7XFLQ^&ct=22^&urls=45.136.21.150^&clientType=13^&appId=512000384397^&id=4289981909641^&bytes=0-52773896200 --output b-audio-%suffix%.webm
+
