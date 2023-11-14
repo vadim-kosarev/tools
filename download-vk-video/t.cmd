332ffc9446ff7e109b1e1598ad0f2488f7c64634
Vadim Kosarev <vadim.kosarev@gmail.com>
2023-11-14 18:33:03 +0300

    pp-67

--- download-vk-video/t.cmd
+++ download-vk-video/t.cmd
@@ -1,3 +1,5 @@
-curl -H @headers.txt https://vkvd42.mycdn.me/?expires=1700415864799^&srcIp=31.173.86.154^&pr=40^&srcAg=CHROME^&ms=45.136.22.132^&type=5^&sig=RD66eYiO21I^&ct=21^&urls=45.136.21.154^&clientType=13^&appId=512000384397^&id=5424406006465^&bytes=0-1000000000 --output a.webm
+set suffix=plohie-pesni-67
 
-curl -H @headers.txt https://vkvd42.mycdn.me/?expires=1700415864799^&srcIp=31.173.86.154^&pr=40^&srcAg=CHROME^&ms=45.136.22.132^&type=1^&sig=he7FbNhLiBI^&ct=22^&urls=45.136.21.154^&clientType=13^&appId=512000384397^&id=5424406006465^&bytes=0-149100000 --output b.webm
+curl -H @headers.txt https://vkvd214.mycdn.me/?expires=1700433809434^&srcIp=31.173.86.154^&pr=40^&srcAg=CHROME^&ms=45.136.22.203^&type=1^&sig=Ga2vUO7XFLQ^&ct=22^&urls=45.136.21.150^&clientType=13^&appId=512000384397^&id=4289981909641^&bytes=0-527738962 --output a-video-%suffix%.webm
+
+curl -H @headers.txt https://vkvd214.mycdn.me/?expires=1700433809434^&srcIp=31.173.86.154^&pr=40^&srcAg=CHROME^&ms=45.136.22.203^&type=6^&sig=SxwmA8rwjz4^&ct=21^&urls=45.136.21.150^&clientType=13^&appId=512000384397^&id=4289981909641^&bytes=0-29594234 --output b-audio-%suffix%.webm
