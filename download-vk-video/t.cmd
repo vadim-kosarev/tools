aef63bf4cd7b82af9651de7c249313bbb6e3db5f
Vadim Kosarev <vadim.kosarev@gmail.com>
2023-11-14 17:49:33 +0300

    Download from vk.com video

---
+++ download-vk-video/t.cmd
@@ -0,0 +1,3 @@
+curl -H @headers.txt https://vkvd42.mycdn.me/?expires=1700415864799^&srcIp=31.173.86.154^&pr=40^&srcAg=CHROME^&ms=45.136.22.132^&type=5^&sig=RD66eYiO21I^&ct=21^&urls=45.136.21.154^&clientType=13^&appId=512000384397^&id=5424406006465^&bytes=0-1000000000 --output a.webm
+
+curl -H @headers.txt https://vkvd42.mycdn.me/?expires=1700415864799^&srcIp=31.173.86.154^&pr=40^&srcAg=CHROME^&ms=45.136.22.132^&type=1^&sig=he7FbNhLiBI^&ct=22^&urls=45.136.21.154^&clientType=13^&appId=512000384397^&id=5424406006465^&bytes=0-149100000 --output b.webm
