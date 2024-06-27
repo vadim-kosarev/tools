049d94c6b6980462915e564b16e2dcb90780e0b6
Vadim Kosarev <vadim.kosarev@gmail.com>
2024-06-27 21:48:55 +0300

    fixed

--- py/g-update-tags.py
+++ py/g-update-tags.py
@@ -7,6 +7,12 @@ import concurrent
 import queue
 from threading import Thread
 from concurrent.futures import ThreadPoolExecutor
+import time
+import json
+import jsonpath
+import subprocess
+import datetime
+
 
 logging.config.fileConfig('logging.ini')
 logger = logging.getLogger(__name__)
@@ -27,6 +33,38 @@ def fileListed(theFilePath):
 def processFile(theFilePath):
     logger = logging.getLogger("processFile")
     logger.info(f"Processing file {theFilePath}")
+    jsonFilePath = f'{theFilePath}.json'
+    with open(jsonFilePath) as f:
+        extJsonData = json.load(f)
+        #print(f'{jsonFilePath} : {extJsonData}')
+
+        logger.info("===")
+        ts = jsonpath.jsonpath(extJsonData, "$..creationTime.timestamp")[0]
+        ts2 = jsonpath.jsonpath(extJsonData, "$..photoTakenTime.timestamp")[0]
+        if not ts2 is None and int(ts2) > 930063138:
+            ts = ts2
+        #print(jsonpath.jsonpath(extJsonData, "$.*.FileModifyDate"))
+        logger.info(ts)
+        if not ts is None and ts != False:
+            imageDate = datetime.datetime.fromtimestamp(float(ts)).strftime("%Y:%m:%d %H:%M:%S")
+            #print("--- " + imageDate)
+
+            cmd1 = f'exiftool "-IFD0:ModifyDate={imageDate}" "{theFilePath}"'
+
+            try:
+                out = subprocess.check_output([
+                    "exiftool",
+                    f'-IFD0:ModifyDate={imageDate}',
+                    "-overwrite_original",
+                    theFilePath
+                ], shell=True)
+                logger.info(cmd1)
+                logger.info(out.decode("utf-8"))
+            except subprocess.CalledProcessError as err:
+                logger.info(f'Process Error: {err}')
+            tsNum = float(ts)
+            os.utime(theFilePath, (tsNum, tsNum))
+            logger.info(f'Updated date: {imageDate}')
 
 def dispatcher():
     global q
@@ -34,9 +72,9 @@ def dispatcher():
     logger = logging.getLogger("dispatcher")
     logger.info("Running dispatcher")
     with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-        while not filesListed:
+        while True:
             logger.info("Getting file from queue")
-            aFile = q.get(block=True, timeout=1)
+            aFile = q.get(block=True)
             logger.info(f'Got {aFile}')
             executor.submit(processFile, aFile)
     logger.info(f"Finished")
@@ -62,6 +100,7 @@ if __name__ == '__main__':
     t1 = Thread(target=dispatcher)
     t2 = Thread(target=listFiles)
     t1.start()
+    time.sleep(5)
     t2.start()
     t1.join()
     t2.join()
