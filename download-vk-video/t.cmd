5851b80faf2a4606f517f618485d3e27b7b01722
Vadim Kosarev <vadim.kosarev@gmail.com>
2025-03-25 15:41:48 +0300

    added join-video with ffmpeg

--- download-vk-video/t.cmd
+++ download-vk-video/t.cmd
@@ -2,17 +2,22 @@
 :: replace ^& with ^^&
 
 set site=boosty
-set suffix=madagaskar-1
+set suffix=MADAGASCAR.paxan_doma
 
-set videoUrl="https://vd331.okcdn.ru/?expires=1742956629653&srcIp=176.115.147.200&pr=42&srcAg=CHROME&ms=185.226.53.40&type=3&sig=IBVFUOKnVMQ&ct=11&urls=45.136.20.28&clientType=18&id=7008066079487&bytes=0-2000000000"
-set audioUrl="https://vd331.okcdn.ru/?expires=1742956629653&srcIp=176.115.147.200&pr=42&srcAg=CHROME&ms=185.226.53.40&type=3&sig=IBVFUOKnVMQ&ct=12&urls=45.136.20.28&clientType=18&id=7008066079487&bytes=0-2000000000"
+set videoUrl="https://vd331.okcdn.ru/?expires=1742982416131&srcIp=185.203.241.169&pr=42&srcAg=CHROME&ms=185.226.53.40&type=3&sig=09AH4hZmYw4&ct=11&urls=45.136.20.28&clientType=18&zs=43&id=7008066079487&bytes=0-2000000000"
+set audioUrl="https://vd331.okcdn.ru/?expires=1742982416131&srcIp=185.203.241.169&pr=42&srcAg=CHROME&ms=185.226.53.40&type=3&sig=09AH4hZmYw4&ct=12&urls=45.136.20.28&clientType=18&zs=43&id=7008066079487&bytes=0-2000000000"
 
 set headersFile=@headers-%site%.txt
-set videoFile=%suffix%-1.VIDEO.webm
-set audioFile=%suffix%-1.AUDIO.webm
+set videoFile=%suffix%.VIDEO.webm
+set audioFile=%suffix%.AUDIO.webm
 
 :: Video
-start "%videoFile%" curl -H %headersFile% %videoUrl% --output "%videoFile%"
+::start "%videoFile%" 
+call curl -H %headersFile% %videoUrl% --output "%videoFile%"
 
 :: Audio
-start "%audioFile%" curl -H %headersFile% %audioUrl% --output "%audioFile%"
+::start "%audioFile%" 
+call curl -H %headersFile% %audioUrl% --output "%audioFile%"
+
+
+ffmpeg -i "%videoFile%" -i "%audioFile%" -c:v copy -c:a copy "%suffix%.FINAL.mp4"
