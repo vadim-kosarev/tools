a9b1a4e0a4ef73095608797a3eadb87e5db4e70b
Vadim Kosarev <vadim.kosarev@gmail.com>
2025-03-25 05:53:38 +0300

    ...

--- download-vk-video/t.cmd
+++ download-vk-video/t.cmd
@@ -1,10 +1,18 @@
 :: update the data below
 :: replace ^& with ^^&
 
-set suffix=zvezdy-1
+set site=boosty
+set suffix=madagaskar-1
+
+set videoUrl="https://vd331.okcdn.ru/?expires=1742956629653&srcIp=176.115.147.200&pr=42&srcAg=CHROME&ms=185.226.53.40&type=3&sig=IBVFUOKnVMQ&ct=11&urls=45.136.20.28&clientType=18&id=7008066079487&bytes=0-2000000000"
+set audioUrl="https://vd331.okcdn.ru/?expires=1742956629653&srcIp=176.115.147.200&pr=42&srcAg=CHROME&ms=185.226.53.40&type=3&sig=IBVFUOKnVMQ&ct=12&urls=45.136.20.28&clientType=18&id=7008066079487&bytes=0-2000000000"
+
+set headersFile=@headers-%site%.txt
+set videoFile=%suffix%-1.VIDEO.webm
+set audioFile=%suffix%-1.AUDIO.webm
 
 :: Video
-curl -H @headers.txt https://vkvd117.mycdn.me/?expires=1710597512403^&srcIp=95.216.148.18^&pr=40^&srcAg=CHROME^&ms=185.226.53.161^&mid=7204095338736^&type=5^&subId=6381682821872^&sig=K0EGottM89U^&ct=32^&urls=45.136.22.176^&clientType=13^&appId=512000384397^&asubs=y^&id=6237076064812^&bytes=0-978873737 --output a-video-%suffix%.webm
+start "%videoFile%" curl -H %headersFile% %videoUrl% --output "%videoFile%"
 
 :: Audio
-curl -H @headers.txt https://vkvd117.mycdn.me/?expires=1710597512403^&srcIp=95.216.148.18^&pr=40^&srcAg=CHROME^&ms=185.226.53.161^&mid=7204095338736^&type=1^&subId=6381682821872^&sig=aKN4Dpo2bzs^&ct=22^&urls=45.136.22.176^&clientType=13^&appId=512000384397^&asubs=y^&id=6237076064812^&bytes=0-166210796 --output b-audio-%suffix%.webm
+start "%audioFile%" curl -H %headersFile% %audioUrl% --output "%audioFile%"
