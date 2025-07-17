029cbe5d1d48b862bcc12baca8b6fb51d6fc0423
Vadim Kosarev <vadim.kosarev@gmail.com>
2025-07-17 08:32:43 +0300

    added pdf_to_png.py

---
+++ py/pdf_to_png.py
@@ -0,0 +1,13 @@
+﻿import fitz  # PyMuPDF
+print(fitz.__file__)
+
+pdf_path = r'O:\Documents\__HOUSE\04. MAIN\725А-s_АС+ИС_Косарева Н,Н_pdf_Freeze.pdf'
+doc = fitz.open(pdf_path)
+
+dpi_target = 600
+scale = dpi_target / 72  # ≈ 8.33
+mat = fitz.Matrix(scale, scale)
+
+for i, page in enumerate(doc):
+    pix = page.get_pixmap(matrix=mat)
+    pix.save(f"page_{i+1}.png")
